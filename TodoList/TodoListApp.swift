106f254cd1aeea75b276c54bd62c572868561337
DeepinderN <deepinderrishi@gmail.com>
2025-07-25 03:30:31 -0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Deepinder on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
